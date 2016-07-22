d554bcea04e40b95b7c798c1d7c48191d477c28e
msahagu2 <msahagu2@gmail.com>
2016-07-22 15:51:44 -0500

    Updated to latest changes in internal repo

--- test/dom/getExecutingScript.test.js
+++ test/dom/getExecutingScript.test.js
@@ -3,23 +3,22 @@
 'use strict';
 
 var getExecutingScript = require('../../lib/dom/getExecutingScript'),
-	expect = require('expect.js');
+	expect             = require('expect.js');
 
-describe('dom/getExecutingScript', function() {
+describe('dom/getExecutingScript', function () {
 
-	it('Should get the script element that loaded the calling javascript.', function(done) {
+	it('Should get the script element that loaded the calling javascript.', function (done) {
 		// We only care about testing modern browsers for this case.
 		// The fallback is handled in the next test.
-		if (!document.currentScript) {
+		if (!'currentScript' in document) {
 			done();
 			return;
 		}
 
 		var el = document.createElement('script');
-
 		el.src = '/base/public/dom-test-verify.js';
 
-		window.getExecutingScriptVerify = function() {
+		window.getExecutingScriptVerify = function () {
 			expect(el).to.equal(getExecutingScript());
 			expect(el.getAttribute(getExecutingScript.LOAD_ATTR)).to.equal(getExecutingScript.LOAD_STARTED);
 
@@ -32,17 +31,16 @@ describe('dom/getExecutingScript', function() {
 		document.body.appendChild(el);
 	});
 
-	it('Should filter the potential executing scripts by the provided detect function.', function(done) {
+	it('Should filter the potential executing scripts by the provided detect function.', function (done) {
 		var el = document.createElement('script');
-
 		el.setAttribute('FOO', 'dummy value');
 		el.src = '/base/public/dom-test-verify.js';
 
-		var detectScriptByAttribute = function(script) {
+		var detectScriptByAttribute = function (script) {
 			return script.getAttribute('FOO') === 'dummy value';
 		};
 
-		window.getExecutingScriptVerify = function() {
+		window.getExecutingScriptVerify = function () {
 			expect(el).to.equal(getExecutingScript(detectScriptByAttribute));
 			el.parentElement.removeChild(el);
 			window.getExecutingScriptVerify = null;
