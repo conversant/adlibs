8c0feabf81f9368985a9f31d9959e2f27a2e10e7
msahagu2 <msahagu2@gmail.com>
2016-07-27 10:51:14 -0500

    Updated to latest commit on internal repo

--- test/dom/getExecutingScript.test.js
+++ test/dom/getExecutingScript.test.js
@@ -6,8 +6,17 @@ var getExecutingScript = require('../../lib/dom/getExecutingScript'),
 	expect             = require('expect.js');
 
 describe('dom/getExecutingScript', function () {
+	it('should be able to handle an invalid currentScript', function() {
+		expect(getExecutingScript(null, 'invalidness')).not.to.be(null);
+		expect(getExecutingScript(null, undefined)).not.to.be(null);
+		expect(getExecutingScript(null, null)).not.to.be(null);
+	});
 
-	it('Should get the script element that loaded the calling javascript.', function (done) {
+	it('should return null if detectScript argument function returns false for all scripts', function() {
+		expect(getExecutingScript(function(){return false;})).to.be(null);
+	});
+
+	it('should get the script element that loaded the calling javascript.', function (done) {
 		// We only care about testing modern browsers for this case.
 		// The fallback is handled in the next test.
 		if (!'currentScript' in document) {
@@ -21,6 +30,7 @@ describe('dom/getExecutingScript', function () {
 		window.getExecutingScriptVerify = function () {
 			expect(el).to.equal(getExecutingScript());
 			expect(el.getAttribute(getExecutingScript.LOAD_ATTR)).to.equal(getExecutingScript.LOAD_STARTED);
+			expect(el.src).to.be.a('string');
 
 			el.parentElement.removeChild(el);
 			window.getExecutingScriptVerify = null;
@@ -31,7 +41,7 @@ describe('dom/getExecutingScript', function () {
 		document.body.appendChild(el);
 	});
 
-	it('Should filter the potential executing scripts by the provided detect function.', function (done) {
+	it('should filter the potential executing scripts by the provided detect function.', function (done) {
 		var el = document.createElement('script');
 		el.setAttribute('FOO', 'dummy value');
 		el.src = '/base/public/dom-test-verify.js';
