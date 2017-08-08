81ee70718573d6c7754fa377594823251df71190
Zhang, Joseph <jozhang@conversantmedia.com>
2017-08-08 13:49:24 -0500

    moved send beacon inside of sendReport

--- test/sendBeacon.test.js
+++ test/sendBeacon.test.js
@@ -12,54 +12,20 @@ describe('sendBeacon', function() {
 });
 
 describe('sendBeacon data types ', function () {
-    if ('navigator' in window && 'sendBeacon' in window.navigator === true) {
-        describe('using native function', function () {
-            it('Try sendBeacon with a string.', function () {
-                expect(sendBeacon('/fake/url', "test string")).to.equal(true);
-            });
+    var settings = {timeout: 10};
 
-            it('Try sendBeacon with a blob.', function () {
-                expect(sendBeacon('/fake/url', new Blob(['abc123'], {type: 'text/plain'}))).to.equal(true);
-            });
-
-            it('Try sendBeacon with an object.', function () {
-                expect(sendBeacon('/fake/url', {fake: 'data'})).to.equal(true);
-            });
-        });
-    }
-
-    else {
-        describe('sendBeacon data types using created function', function () {
-            it('Try sendBeacon with a string.', function () {
-                expect(sendBeacon('/fake/url', "test string")).to.equal(true);
-            });
-
-            it('Try sendBeacon with a blob.', function () {
-                expect(sendBeacon('/fake/url', new Blob(['abc123'], {type: 'text/plain'}))).to.equal(true);
-            });
-
-            it('Try sendBeacon with an object.', function () {
-                expect(sendBeacon('/fake/url', {fake: 'data'})).to.equal(true);
-            });
-        });
-    }
-});
-
-describe('sendBeacon with a settings object', function() {
-    it('Try sendBeacon with a settings object.', function() {
-        var settings = {timeout: 10};
-        if ('navigator' in window && 'sendBeacon' in window.navigator === true && typeof(settings) === 'object') {
-            expect(sendBeacon('/fake/url', "test string", settings)).to.equal(true);
-        } else {
-            expect(sendBeacon('/fake/url', "test string", settings)).to.equal(true);
-        }
+    it('Try sendBeacon with a string.', function () {
+        expect(sendBeacon('/fake/url', String('asdf'))).to.equal(true);
+        expect(sendBeacon('/fake/url', String('asdf'), settings)).to.equal(true);
     });
-});
 
-describe('sendBeacon with a settings object', function() {
-    it('Try sendBeacon with a settings object.', function() {
-        if (!('navigator' in window && ('sendBeacon' in window.navigator)) && 'XMLHttpRequest' in window === true) {
-            expect(sendBeacon('/fake/url', "test string", {timeout: 10})).to.equal(true);
-        }
+    it('Try sendBeacon with a blob.', function () {
+        expect(sendBeacon('/fake/url', new Blob(['abc123'], {type: 'text/plain'}))).to.equal(true);
+        expect(sendBeacon('/fake/url', new Blob(['abc123'], {type: 'text/plain'}), settings)).to.equal(true);
+    });
+
+    it('Try sendBeacon with an object.', function () {
+        expect(sendBeacon('/fake/url', {fake: 'data'})).to.equal(true);
+        expect(sendBeacon('/fake/url', {fake: 'data'}, settings)).to.equal(true);
     });
 });
