272a45674efdf5ee605d661c65318223df5d5fa8
Zhang, Joseph <jozhang@conversantmedia.com>
2017-08-08 10:56:53 -0500

    updated comments

--- test/sendBeacon.test.js
+++ test/sendBeacon.test.js
@@ -30,41 +30,36 @@ describe('sendBeacon data types ', function () {
 
     else {
         describe('sendBeacon data types using created function', function () {
-            if ('navigator' in window && 'sendBeacon' in window.navigator === false) {
-                it('Try sendBeacon with a string.', function () {
-                    expect(sendBeacon('/fake/url', "test string")).to.equal(true);
-                });
+            it('Try sendBeacon with a string.', function () {
+                expect(sendBeacon('/fake/url', "test string")).to.equal(true);
+            });
 
-                it('Try sendBeacon with a blob.', function () {
-                    expect(sendBeacon('/fake/url', new Blob(['abc123'], {type: 'text/plain'}))).to.equal(true);
-                });
+            it('Try sendBeacon with a blob.', function () {
+                expect(sendBeacon('/fake/url', new Blob(['abc123'], {type: 'text/plain'}))).to.equal(true);
+            });
 
-                it('Try sendBeacon with an object.', function () {
-                    expect(sendBeacon('/fake/url', {fake: 'data'})).to.equal(true);
-                });
-            }
+            it('Try sendBeacon with an object.', function () {
+                expect(sendBeacon('/fake/url', {fake: 'data'})).to.equal(true);
+            });
         });
     }
 });
 
 describe('sendBeacon with a settings object', function() {
     it('Try sendBeacon with a settings object.', function() {
-        if ('navigator' in window && 'sendBeacon' in window.navigator === true) {
-            expect(sendBeacon('/fake/url', "test string", {timeout: 10})).to.equal(true);
+        var settings = {timeout: 10};
+        if ('navigator' in window && 'sendBeacon' in window.navigator === true && typeof(settings) === 'object') {
+            expect(sendBeacon('/fake/url', "test string", settings)).to.equal(true);
         } else {
-            expect(sendBeacon('/fake/url', "test string", {timeout: 10})).to.equal(true);
+            expect(sendBeacon('/fake/url', "test string", settings)).to.equal(true);
         }
     });
 });
 
 describe('sendBeacon with a settings object', function() {
     it('Try sendBeacon with a settings object.', function() {
-        if ('navigator' in window && 'sendBeacon' in window.navigator !== true) {
-            if ('XMLHttpRequest' in window === true) {
-                expect(sendBeacon('/fake/url', "test string", {timeout: 10})).to.equal(true);
-            } else {
-                expect(sendBeacon('/fake/url', "test string", {timeout: 10})).to.equal(true);
-            }
+        if (!('navigator' in window && ('sendBeacon' in window.navigator)) && 'XMLHttpRequest' in window === true) {
+            expect(sendBeacon('/fake/url', "test string", {timeout: 10})).to.equal(true);
         }
     });
 });
