23f3a57f72f0d3a427dd17888e961c93c6684b12
Zhang, Joseph <jozhang@conversantmedia.com>
2017-08-23 14:06:36 -0500

    Utilize sendBeacon when supported

--- test/parseConfig.test.js
+++ test/parseConfig.test.js
@@ -3,19 +3,15 @@
 'use strict';
 
 var parseConfig = require('../lib/parseConfig'),
-	JSON3 = require('json3'),
+    forIn = require('../lib/canHas').forIn,
     expect = require('expect.js');
-
-// required so test will pass in old IE and Quirks Mode
-// run in noConflict mode so it doesn't touch window.JSON
-JSON3.noConflict();
 
 var CONFIG_ATTR = 'data-cnvr-foo';
 
 function createElement(data) {
     var el = document.createElement('script');
 
-	el.setAttribute(CONFIG_ATTR, JSON3.stringify(data));
+	el.setAttribute(CONFIG_ATTR, JSON.stringify(data));
 
     return el;
 }
@@ -38,20 +34,6 @@ describe('parseConfig', function() {
         expect(config.type).to.equal('full');
     });
 
-	it('Should fallback to eval for parsing when native JSON support is missing.', function() {
-		var nativeJSON = typeof window.JSON !== 'undefined' && JSON.parse;
-
-		if (nativeJSON) {
-			window.JSON = undefined;
-		}
-
-		var config = parseConfig(createElement({ foo: 'bar' }), CONFIG_ATTR);
-
-		expect(config.foo).to.equal('bar');
-
-		window.JSON = nativeJSON;
-	});
-
 	it('Should apply defaults to missing values in the config.', function() {
 		var config = parseConfig(createElement({
 			trx: 'a2z',
@@ -62,5 +44,4 @@ describe('parseConfig', function() {
 		expect(config.url).to.equal('http://foo.bar');
 		expect(config.align).to.equal('center');
 	});
-
 });
